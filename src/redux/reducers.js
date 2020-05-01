f9aa91f34cb2aa8781206bbca870489cf6d38910
1t1e1 <1t1ebir@gmail.com>
2020-05-01 03:35:28 +0300

    make default state

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,4 +1,22 @@
-export const todoReducer = (state = [], action) => {
+const DEFAULT_STATE = [
+    {
+        id: Math.random(),
+        completed: true,
+        content: "decide what to learn",
+    },
+    {
+        id: Math.random(),
+        completed: false,
+        content: "learn react",
+    },
+    {
+        id: Math.random(),
+        completed: false,
+        content: "learn redux",
+    },
+];
+
+export const todoReducer = (state = DEFAULT_STATE, action) => {
     switch (action.type) {
         case "ADD_TODO":
             return [
